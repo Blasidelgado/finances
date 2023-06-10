339bb5f13f2c1895b21184abbd445eb4629ed09d
Blasidelgado <blasidelgado@gmail.com>
2023-06-09 22:43:28 -0300

    first commit

---
+++ helpers.py
@@ -0,0 +1,86 @@
+import os
+import requests
+import urllib.parse
+
+import re
+
+from flask import redirect, render_template, request, session
+from functools import wraps
+
+
+def apology(message, code=400):
+    """Render message as an apology to user."""
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+def usd(value):
+    """Format value as USD."""
+    return f"${value:,.2f}"
+
+
+def lookup(symbol):
+    """Look up quote for symbol."""
+
+    # Contact API
+    try:
+        api_key = os.environ.get("API_KEY")
+        url = f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}"
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.RequestException:
+        return None
+
+    # Parse response
+    try:
+        quote = response.json()
+        return {
+            "name": quote["companyName"],
+            "price": float(quote["latestPrice"]),
+            "symbol": quote["symbol"]
+        }
+    except (KeyError, TypeError, ValueError):
+        return None
+
+
+# Custom function to validate password
+def validate_password(password):
+    # Validate password to have 8 or more characters
+    if len(password) < 8:
+        return False
+    # Validate password to have at least one lowercase letter
+    if not re.search("[a-z]", password):
+        return False
+    # Validate password to have at least one uppercase letter
+    if not re.search("[A-Z]", password):
+        return False
+    # Validate password to have at least one number
+    if not re.search("[0-9]", password):
+        return False
+    # Validate password to have at least one symbol
+    if not re.search("[@#$%^&+=]", password):
+        return False
+    return True
